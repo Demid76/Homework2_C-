0604efdfd225cc0be188d884be0913d9cfa3112c
Andrey_Demidov <a_demidov91@mail.ru>
2022-07-18 22:08:03 +0300

    Seminar2 tasks

---
+++ Seminar2/Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(NumberA);
+Console.WriteLine("Вторая цифра введённого числа =" +stringNumber[1]);
+
---
+++ Seminar2/Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите порядковый номер дня недели: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+  if (Number == 6 || Number == 7) {
+  Console.WriteLine("этот день выходной ");
+  }
+  else if (Number < 1 || Number > 7) {
+    Console.WriteLine("Это не день недели ");
+  }
+  else{
+  Console.WriteLine("этот день не выходной ");
+  }
